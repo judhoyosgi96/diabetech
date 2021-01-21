10e8deb2381d2c5b9f32c1b9db750a14e9decab1
DESKTOP-AGRBT6B\j_u_a <juan.hoyos@bankami.co>
2021-01-21 18:48:50 -0500

    solve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CheckTutorial } from './providers/check-tutorial.service';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/app/tabs/schedule',
     pathMatch: 'full'
   },
   {
